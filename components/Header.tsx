540f249457df4828f9c2dcc0da0e5b4f204961b7
Sean Zou <wersling@gmail.com>
2025-11-18 13:11:43 +0800

    Replace logo SVG with Next.js Image component for improved performance and remove unused logo.svg file.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Image from 'next/image'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -17,7 +17,13 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Logo />
+            <Image
+              src="/static/favicons/apple-touch-icon.png"
+              alt="Logo"
+              width={40}
+              height={40}
+              className="rounded-lg"
+            />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
